acc6a1084d4f96e99b7e3d3d46d64d9f97ae8829
IvanImen <vanyagarret@gmail.com>
2023-11-11 02:37:54 +0200

    createBookCard_and_markup

--- src/js/book-popup.js
+++ src/js/book-popup.js
@@ -1,38 +1,73 @@
-// function createMarkup(arr) {
-//   return arr
-//     .map(
-//       ({ cover, title, author, descr }) => `
-//   <div class="book-image">
-//     <img class="img-book-cover" src="" alt="" />
-//   </div>
-//   <div>
-//     <h2 class="book-title"></h2>
-//     <p class="book-author"></p>
-//     <p class="book-descr"></p>
-//     <ul>
-//       <li><img class="book-store" src="" alt="" /></li>
-//       <li><img class="book-img" src="" alt="" /></li>
-//     </ul>
-//   </div>
-//   <button class="add-to-list"></button>
-//   `
-//     )
-//     .join('');
-// }
+// Імпорт функцій
+import { fetchBookById } from './api';
 
-getBookById(bookId)
-  .then(data => {
-    const url = data[0].url;
-    const name = data[0].breeds[0].name;
-    const description = data[0].breeds[0].description;
-    const temperament = data[0].breeds[0].temperament;
+// Забираю з HTML елемент модальне вікно та елемент кнопка
 
-    el.catCard.innerHTML = createCatMarkup(url, name, description, temperament);
-    el.catCard.classList.remove('js-hidden');
-  })
-  .catch(error => {
-    Notify.failure(`Error: ${error}`);
-  })
-  .finally(() => {
-    el.loader.classList.add('js-hidden');
-  });
+const el = {
+  modalWindow: document.querySelector('.modal-window'),
+  buttonAddToList: document.querySelector('.add-to-list'),
+};
+
+function createBookCard(bookId) {
+  // Початок відображення спінеру
+  fetchBookById(bookId)
+    .then(({ book_image, title, author, description, buy_links }) => {
+      const amazon_link = buy_links[0].url;
+      const appleBooks_link = buy_links[1].url;
+      console.log(book_image);
+      console.log(title);
+      console.log(author);
+      console.log(description);
+      console.log(amazon_link);
+      console.log(appleBooks_link);
+      el.modalWindow.innerHTML = createMarkup(
+        book_image,
+        title,
+        author,
+        description,
+        amazon_link,
+        appleBooks_link
+      );
+    })
+    .catch(error => {
+      Notify.failure(`Error: ${error}`);
+    })
+    .finally(() => {
+      // Відключення спінеру
+    });
+}
+
+function createMarkup(
+  bookCover,
+  title,
+  author,
+  descr,
+  amazonLink,
+  appleBooksLink
+) {
+  return `
+    <div class="book-image">
+      <img class="img-book-cover" src="${bookCover}" alt="${title}" />
+    </div>
+    <div>
+      <h2 class="book-title">${title}</h2>
+      <p class="book-author">${author}</p>
+      <p class="book-descr">${descr}</p>
+    <ul>
+      <li>
+        <a href="${amazonLink}">
+        <img src="../images/book-popup/amazon.svg" alt="" />
+        </a>
+      </li>
+      <li>
+        <a href="${appleBooksLink}">
+        <img src="../images/book-popup/appleBooks.svg" alt="" />
+        </a>
+      </li>
+    </ul>
+    </div>
+    <button class="add-to-list"></button>
+    `;
+}
+
+createBookCard('643282b2e85766588626a144');
