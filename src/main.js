db6a07a5c7b292682b970174d54bb9e410abbc75
andyfullstack <andriikoliadenko@gmail.com>
2023-11-11 23:34:20 +0200

    upd

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import './js/auth-popup';
 import './js/auth-firebase';
 import './js/book-popup';
 import './js/btn-scroll-up';
+import './js/category-list-sec';
 import './js/top-books-sec';
 import './js/shopping-list-sec';
-import './js/category-list-sec';
 import './js/header';
 import './js/loader';
 import './js/mobile-menu';
@@ -13,4 +13,3 @@ import './js/refs';
 import './js/selected-category-sec';
 import './js/support-ukraine-sec';
 import './js/theme-dark-light';
-
