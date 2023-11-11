fed9d59bf57b5bffb3530ad67432f40785b4b52b
roLLlyTHuK <ircvipua@gmail.com>
2023-11-11 15:23:48 +0200

    hidding game

--- src/js/header.js
+++ src/js/header.js
@@ -17,22 +17,27 @@ document.addEventListener('DOMContentLoaded', function () {
       item.classList.add('active-page');
     }
   });
-  headerUserName.textContent = 'Sign up';
-  headerUserLogo.classList.add('visually-hidden');
-  getNameForUpdateHeaderUser();
-  let isAuthenticated = checkAuthenticationStatus();
-  let menuAuthIcon = document.querySelector('.menu-auth-icon use');
-  isAuthenticated
-    ? menuAuthIcon.setAttribute(
+  // headerUserName.textContent = 'Sign up';
+  // headerUserLogo.classList.add('visually-hidden');
+  getNameForUpdateHeaderUser().then(login => {
+    console.log(login);
+    let menuAuthIcon = document.querySelector('.menu-auth-icon use');
+    let authLink = document.querySelector('.auth-link');
+    if (login) {
+      menuAuthIcon.setAttribute(
         'href',
         '/images/header/header-defs.svg#icon-fi-ss-caret-down-1'
-      )
-    : menuAuthIcon.setAttribute(
+      );
+      authLink.classList.remove('visually-hidden');
+    } else {
+      menuAuthIcon.setAttribute(
         'href',
         '/images/header/header-defs.svg#icon-arrow-narrow-right-1'
       );
+      authLink.classList.add('visually-hidden');
+    }
+  });
 });
-
 const headerUser = document.querySelector('.menu-auth');
 const authModal = document.querySelector('.auth-modal');
 const headerUserName = document.querySelector('.menu-username');
@@ -41,11 +46,13 @@ const headerUserLogo = document.querySelector('.menu-auth-logo');
 //отримуємо ім'я користувача
 async function getNameForUpdateHeaderUser() {
   const login = await isAuthUser();
+  console.log(login);
   if (login) {
     getUserName().then(getUserNameRes => {
       updateHeaderUser(getUserNameRes.data().name);
     });
   }
+  return login;
 }
 //запуск модального вікна
 headerUser.addEventListener('click', () => {
