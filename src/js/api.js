f84afcacf42d84d1fd2fb64b6c6e1655eb21bb45
IvanImen <vanyagarret@gmail.com>
2023-11-11 02:28:32 +0200

    bookCard_and_markup

--- src/js/api.js
+++ src/js/api.js
@@ -14,9 +14,6 @@ async function fetchBookList() {
 }
 fetchBookList();
 
-
-
-
 /**
  * запит на отримання топ 5 книг у кожній категорії
  * @returns Array of top books
@@ -27,10 +24,6 @@ async function fetchTopBooks() {
   return resp;
 }
 fetchTopBooks();
-
-
-
-
 
 /**
  * функція приймає назву категорії (формат стрінг), повертає масив книг з повною інформацією
@@ -45,11 +38,6 @@ async function fetchBooksOfCategory(category) {
 }
 fetchBooksOfCategory('Childrens Middle Grade Hardcover');
 
-
-
-
-
-
 /**
  * функція приймає ID обраної книги у форматі стрінги і повертає масив інфо
  * @param {string} bookId
@@ -61,8 +49,5 @@ async function fetchBookById(bookId) {
   return resp;
 }
 fetchBookById('643282b1e85766588626a0dc');
-
-
-
 
 export { fetchBookById, fetchBookList, fetchTopBooks, fetchBooksOfCategory };
