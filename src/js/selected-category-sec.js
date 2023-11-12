6d1e23e901068147ec7dc5368b160c900da11d06
Lakanna <annalakus@ukr.net>
2023-11-12 17:21:24 +0000

    responsiv

--- src/js/selected-category-sec.js
+++ src/js/selected-category-sec.js
@@ -1,79 +1,61 @@
-import Notiflix from 'notiflix';
-
-import { fetchBooksOfCategory } from '../js/api.js'
+import { fetchBooksOfCategory } from './api.js';
+import { createBookCard } from './book-popup';
 import { el } from './refs.js';
-
 
 
 
 /**
  * Функція приймає параметром назву категорії у вигляді стрінги, робить запит на бекенд, створює розмітку і рендерить
- * @param {string} category 
+ * @param {string} category
  */
-async function createSelectedCategory(category){
-
-    try {
-    
-    const arrOfBooks = await fetchBooksOfCategory(category)
+async function createSelectedCategory(category) {
+  try {
+    const arrOfBooks = await fetchBooksOfCategory(category);
     const markUp = createMarkupCategory(arrOfBooks);
     const styledTitle = styleSectionTitle(category);
-        
-    el.categoryBox.insertAdjacentHTML("afterbegin", markUp);
-          
-    el.categoryTitle.innerHTML = styledTitle;
 
-  
-} catch (error) {
+    el.list.innerHTML =
+      `<h1 class="selected-category-title js-category-title">${styledTitle}</h1><ul class="selected-category-list js-category-box">` +
+      markUp + '</ul>';
+      
+    el.categoryBox = document.querySelector('.js-category-box');
+    el.categoryBox.addEventListener('click', createBookCard);
+  } catch (error) {
     console.log(error);
     // Notiflix.Notify.info('Sorry, there are no books matching your search query. Please try again.')
+  }
 }
-}
-
-createSelectedCategory("Hardcover Fiction");
-
-el.categoryBox.addEventListener("click", handlerSelectBook);
-
-
-
-function handlerSelectBook(evt) {
-    if (evt.target === evt.currentTarget){
-        return;
-    }
-    
-    const currentBook = evt.target.closest(".card-wrapper");
-    const currentBookId = currentBook.dataset.id;
-    console.dir(currentBookId);
-    return currentBookId;
-}
-
 
 
 function styleSectionTitle(title) {
-    const words = title.split(" ");
-    const halfIndex = Math.ceil(words.length / 2);
-    const firstHalf = words.slice(0, halfIndex).join(" ");
-    const secondHalf = words.slice(halfIndex).join(" ");
+  const words = title.split(' ');
+  const halfIndex = Math.ceil(words.length / 2);
+  const firstHalf = words.slice(0, halfIndex).join(' ');
+  const secondHalf = words.slice(halfIndex).join(' ');
 
-    return `
+  return `
         ${firstHalf} <span class="second-half">${secondHalf}</span>
     `;
-};
+}
 
 
 
 function createMarkupCategory(arr) {
-    return arr.map(({author, title, book_image, _id}) => {
-        return `<li class="card-wrapper" data-id="${_id}">
-                   <div class="book-img">
+  return arr
+    .map(({ author, title, book_image, _id }) => {
+      return `<li class="card-wrapper  js-book-on-click" data-id="${_id}">
+                   <div selected-category-book-img">
                       <img src="${book_image}" alt="${title}">
                    </div>
-                   <h2 class="book-title">${title}</h2>
-                   <p class="book-author">${author}</p>
+                   <h2 class="selected-category-book-title">${title}</h2>
+                   <p class="selected-category-book-author">${author}</p>
                 </li>
-   `})
-    .join("")
-};
+   `;
+    })
+    .join('');
+}
 
+createSelectedCategory();
 
 
 export { createSelectedCategory };
