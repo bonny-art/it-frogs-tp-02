6d98502536fdc11616ec7dd58c440866bd826bd1
mihaillaur <mihaillaur879@gmail.com>
2023-11-12 22:57:33 +0200

    mob-menu-update

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,14 +1,13 @@
 import { doc } from 'firebase/firestore';
 import { logOutUser,} from './auth-firebase';
-
 const btnOpenMenu = document.getElementById("open-mobile-menu");
 const mobMenu = document.querySelector(".js-mobile-menu");
 const btnHome = document.querySelector('.button-home');
 const btnShoppingList = document.querySelector('.button-shopping-list');
 const btnLogOut = document.querySelector('.button-log-out');
-console.log(btnOpenMenu);
-console.log(mobMenu);
-console.log(btnHome);
+// console.log(btnOpenMenu);
+// console.log(mobMenu);
+// console.log(btnHome);
 
 btnOpenMenu.addEventListener('click', openMobMenu);
 btnHome.addEventListener('click', goHome);
