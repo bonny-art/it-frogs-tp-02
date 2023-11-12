3167af6c1f7e0dcb626466a478d3d58d0796a395
bonny-art <svitlana.otenko@gmail.com>
2023-11-12 03:26:32 +0200

    changes

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,16 @@
 const el = {
   categoryTitle: document.querySelector('.js-category-title'),
   categoryBox: document.querySelector('.js-category-box'),
+
   shoppingList: document.querySelector('.js-shopping-list'),
   emptyPage: document.querySelector('.js-empty-page'),
+
+  body: document.querySelector('body'),
+  toggleBtn: document.querySelector('.tgl-btn'),
+  toggleIcon: document.querySelector('.tgl-btn__icon'),
+
+  bookPopup: document.querySelector('.modal-window'),
+  buttonAddToList: '',
 };
 
 export { el };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import './js/theme-dark-light';
 import './js/api';
 import './js/auth-popup';
 import './js/auth-firebase';
 import './js/book-popup';
 import './js/btn-scroll-up';
 import './js/top-books-sec';
+import './js/support-ukraine-sec';
 import './js/category-list-sec';
 import './js/shopping-list-sec';
 import './js/header';
@@ -11,5 +13,3 @@ import './js/loader';
 import './js/mobile-menu';
 import './js/refs';
 import './js/selected-category-sec';
-import './js/support-ukraine-sec';
-import './js/theme-dark-light';
