7c70aabcd0bc47e2afc4991a61767e69bf31c381
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2023-11-12 20:44:45 +0200

    qwerty

--- src/js/top-books-sec.js
+++ src/js/top-books-sec.js
@@ -1,8 +1,10 @@
+// import fetchTopBooks from api.js
 async function createTopBooks() {
   try {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'block'
     const result = await fetch('https://books-backend.p.goit.global/books/top-books');
     const bookData = await result.json();
-    console.log(bookData);
 
     const markup = bookData.map((book, index) => `
       <div>
@@ -11,32 +13,46 @@ async function createTopBooks() {
           <ul class="list-item swiper-wrapper">
             ${book.books.slice(0, 5).map((book, i) => `
               <li class="li-top js-click-on-book swiper-slide" data-id="${index}-${i}">
-                <img class="img-top" src="${book.book_image}" alt="" />
+               <img class="img-top" src="${book.book_image || './images/shopping_list_sec/empty_page_mobile_2x.png'}" alt="Зображення відсутнє" />
                 <p class="top-bookTitle">${book.title}</p>
                 <p class="top-bookAuthor">${book.author}</p>
               </li>`).join("")}
           </ul>
         </div>
-        <div class="top-button">
+      </div>
+      <div class="top-button">
           <button class="top-bth top-bth-js" type="submit">see more</button>
         </div>
-      </div>`).join("");
+      `).join("");
 
     const list = document.getElementById('list');
     list.innerHTML = markup;
+    // создана кнопка See more и функция handleClick для логики нажатия на кнопку
+  const button = document.querySelectorAll(".top-bth-js");
+  button.forEach((button) => {
+      button.addEventListener("click", handleClick);
+    });
 
   } catch (error) {
     console.error('Помилка отримання або обробки даних:', error);
     // Notiflix.Notify.failure('Помилка отримання або обробки даних:', error);
   }
-
-  const button = document.querySelector(".top-bth-js");
-  button.addEventListener("click", handleClick);
+  finally {
+     const loader = document.getElementById('loader');
+     loader.style.display = 'none'
+   }
+  
 }
-
+// функция для логики нажатия на кнопку see more
 function handleClick() {
-  location.reload();
+  // для проверки работает она или нет поставил перезагружаться по нажатию
+  location.reload(); 
 }
 
 createTopBooks();
+
+
+
+
+
 
