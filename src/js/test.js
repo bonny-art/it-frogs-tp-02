70ca9ef3b9491c6250fb1686794a374bc7a2b2b8
bonny-art <svitlana.otenko@gmail.com>
2023-11-13 15:19:06 +0200

    test 2

--- src/js/test.js
+++ src/js/test.js
@@ -1,11 +1,13 @@
-const test = document.querySelector(
-  '.test-input-icon use[href="/images/header/header-defs.svg#icon-mail-1"]'
-);
+// const test = document.querySelector(
+//   '.test-input-icon use[href="/images/header/header-defs.svg#icon-mail-1"]'
+// );
 
-console.log('test', test);
+// console.log('test', test);
 
-const aaa = test.parentNode;
+// const aaa = test.parentNode;
 
-console.log('parentNode', aaa);
+// console.log('parentNode', aaa);
 
-aaa.classList.add('is-hidden');
+// aaa.classList.add('is-hidden');
+
+// <use href="/it-frogs-tp-02/assets/header-defs-006b49d6.svg#icon-mail-1"></use>;
