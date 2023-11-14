ec04bf30b12bf5d2aec9a3e2463947b4bf2237cd
mihaillaur <mihaillaur879@gmail.com>
2023-11-14 04:03:39 +0200

    mobile

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,34 +1,69 @@
 import { doc } from 'firebase/firestore';
-import { logOutUser,} from './auth-firebase';
-const btnOpenMenu = document.getElementById("open-mobile-menu");
-const mobMenu = document.querySelector(".js-mobile-menu");
+import { logOutUser } from './auth-firebase';
+import { handlerActionAuth, initAuthModal } from './auth-popup';
+import { getNameForUpdateHeaderUser } from './header';
+
+const mobMenu = document.querySelector('.js-mobile-menu');
 const btnHome = document.querySelector('.button-home');
 const btnShoppingList = document.querySelector('.button-shopping-list');
 const btnLogOut = document.querySelector('.button-log-out');
+const mobUserName = document.querySelector('.mob-user-signed');
+const btnSignUp = document.querySelector('.mob-sign-up');
 // console.log(btnOpenMenu);
 // console.log(mobMenu);
 // console.log(btnHome);
-console.log('hello');
-btnOpenMenu.addEventListener('click', openMobMenu);
 btnHome.addEventListener('click', goHome);
 btnShoppingList.addEventListener('click', openShoppingList);
 btnLogOut.addEventListener('click', logOut);
+// btnSignUp.addEventListener('click', handlerBtnSingUp);
 
-function openMobMenu(evt){
-mobMenu.style.display = 'block'
+// function handlerBtnSingUp(){
+//     initAuthModal();
+//     handlerActionAuth();
+// }
+getNameForUpdateHeaderUser().then(login => {
+  if (login) {
+    btnSignUp.classList.add('visually-hidden');
+    btnHome.classList.remove('visually-hidden');
+    btnShoppingList.classList.remove('visually-hidden');
+    btnLogOut.classList.remove('visually-hidden');
+    mobUserName.classList.remove('visually-hidden');
+    console.log(mobUserName);
+
+  } else {
+    btnHome.classList.add('visually-hidden');
+    btnShoppingList.classList.add('visually-hidden');
+    btnLogOut.classList.add('visually-hidden');
+    btnSignUp.classList.remove('visually-hidden');
+    mobUserName.classList.add('visually-hidden');
+  }
+});
+getNameForUpdateHeaderUser()
+const authModal = document.querySelector('.auth-modal');
+btnSignUp.addEventListener('click', () => {
+  authModal.classList.remove('visually-hidden');
+mobMenu.style.display = 'none';
+  initAuthModal();
+  handlerActionAuth();
+});
+
+function goHome(evt) {
+  window.location.href = '/index.html';
+  mobMenu.style.display = 'none';
 }
 
-function goHome(evt){
-    window.location.href = '/index.html'
-    mobMenu.style.display = 'none'
+function openShoppingList(evt) {
+  window.location.href = 'shopping-list.html';
 }
 
-function openShoppingList(evt){
-    window.location.href = 'shopping-list.html'
+function logOut(evt) {
+  logOutUser();
+  window.location.href = '/index.html';
 }
 
-function logOut(evt){
-    logOutUser()
-window.location.href = '/index.html'
-}
-
+// const authModal = document.querySelector('.auth-modal');
+// переменнаяКнопки.addEventListener('click', () => {
+//   authModal.classList.remove('visually-hidden');
+//   initAuthModal();
+//   handlerActionAuth();
+// });
