974517a8d523a9bd0b627981df7afc993b4812cb
roLLlyTHuK <ircvipua@gmail.com>
2023-11-16 18:47:37 +0200

    mob index+styles

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,46 +1,45 @@
-import { doc } from 'firebase/firestore';
 import { logOutUser } from './auth-firebase';
 import { handlerActionAuth, initAuthModal } from './auth-popup';
 import { getNameForUpdateHeaderUser } from './header';
-// import svgSprite from '/images/mobile-menu/mobile-menu-defs.svg';
 
 const mobMenu = document.querySelector('.js-mobile-menu');
-const btnHome = document.querySelector('.button-home');
-const btnShoppingList = document.querySelector('.button-shopping-list');
 const btnLogOut = document.querySelector('.button-log-out');
 const mobUserName = document.querySelector('.mob-user-signed');
 const btnSignUp = document.querySelector('.mob-sign-up');
 const mobSignIn = document.querySelector('.mob-sign-in');
-// console.log(btnOpenMenu);
-// console.log(mobMenu);
-// console.log(btnHome);
-btnHome.addEventListener('click', goHome);
-btnShoppingList.addEventListener('click', openShoppingList);
+const shoppingListLink = document.querySelector('.mob-auth-link');
 btnLogOut.addEventListener('click', logOut);
-// btnSignUp.addEventListener('click', handlerBtnSingUp);
 
-// console.log(getNameForUpdateHeaderUser());
-let mobLogOutIcon = document.querySelector('.mob-log-out-icon')
-console.log(mobLogOutIcon);
+let mobLogOutIcon = document.querySelector('.mob-log-out-icon');
+
+document.addEventListener('DOMContentLoaded', function () {
+  let menuItems = document.querySelectorAll('.mob-nav-item');
+  switch (el.body.id) {
+    case 'index':
+      menuItems[0].classList.add('mob-active-page');
+      break;
+    case 'shopping':
+      menuItems[1].classList.add('mob-active-page');
+      break;
+
+    default:
+      menuItems[0].classList.add('mob-active-page');
+  }
+});
+
+// console.log(mobLogOutIcon);
 getNameForUpdateHeaderUser().then(login => {
   if (login) {
     btnSignUp.classList.add('visually-hidden');
-    btnHome.classList.remove('visually-hidden');
-    btnShoppingList.classList.remove('visually-hidden');
     btnLogOut.classList.remove('visually-hidden');
     mobUserName.classList.remove('visually-hidden');
-    // mobLogOutIcon.setAttribute(
-    //   'href',
-    //   `${svgSprite}#icon-arrow-narrow-right-1`
-    // );
+    shoppingListLink.classList.remove('visually-hidden');
   } else {
-    btnHome.classList.add('visually-hidden');
-    btnShoppingList.classList.add('visually-hidden');
     btnLogOut.classList.add('visually-hidden');
     btnSignUp.classList.remove('visually-hidden');
     mobUserName.classList.add('visually-hidden');
     mobSignIn.classList.add('visually-hidden');
-   
+    shoppingListLink.classList.add('visually-hidden');
   }
 });
 getNameForUpdateHeaderUser();
@@ -52,23 +51,6 @@ btnSignUp.addEventListener('click', () => {
   handlerActionAuth();
 });
 
-function goHome(evt) {
-  window.location.href = '/index.html';
-  mobMenu.style.display = 'none';
-}
-
-function openShoppingList(evt) {
-  window.location.href = 'shopping-list.html';
-}
-
 function logOut(evt) {
   logOutUser();
-  // window.location.href = '/index.html';
 }
-
-// const authModal = document.querySelector('.auth-modal');
-// переменнаяКнопки.addEventListener('click', () => {
-//   authModal.classList.remove('visually-hidden');
-//   initAuthModal();
-//   handlerActionAuth();
-// });
